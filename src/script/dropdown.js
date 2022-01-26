ede449aa7be66f2048d5301ec0b9ce2228aee600
Ruslan Shamsutdinov <Ruslan_Shamsutdinov@epam.com>
2022-01-26 19:57:18 +0300

    refactor code and add functions for IE9

--- src/script/dropdown.js
+++ src/script/dropdown.js
@@ -1,99 +1,36 @@
-if (!String.prototype.startsWith) {
-  Object.defineProperty(String.prototype, 'startsWith', {
-    value: function(search, rawPos) {
-      var pos = rawPos > 0 ? rawPos|0 : 0;
-      return this.substring(pos, pos + search.length) === search;
-    }
-  });
-}
-(function() {
-  // helpers
-  var regExp = function(name) {
-    return new RegExp('(^| )'+ name +'( |$)');
-  };
-  var forEach = function(list, fn, scope) {
-    for (var i = 0; i < list.length; i++) {
-      fn.call(scope, list[i]);
-    }
-  };
-
-  // class list object with basic methods
-  function ClassList(element) {
-    this.element = element;
-  }
-
-  ClassList.prototype = {
-    add: function() {
-      forEach(arguments, function(name) {
-        if (!this.contains(name)) {
-          this.element.className += ' '+ name;
-        }
-      }, this);
-    },
-    remove: function() {
-      forEach(arguments, function(name) {
-        this.element.className =
-          this.element.className.replace(regExp(name), '');
-      }, this);
-    },
-    toggle: function(name) {
-      return this.contains(name)
-        ? (this.remove(name), false) : (this.add(name), true);
-    },
-    contains: function(name) {
-      return regExp(name).test(this.element.className);
-    },
-    // bonus..
-    replace: function(oldName, newName) {
-      this.remove(oldName), this.add(newName);
-    }
-  };
-
-  // IE8/9, Safari
-  if (!('classList' in Element.prototype)) {
-    Object.defineProperty(Element.prototype, 'classList', {
-      get: function() {
-        return new ClassList(this);
-      }
-    });
-  }
-
-  // replace() support for others
-  if (window.DOMTokenList && DOMTokenList.prototype.replace == null) {
-    DOMTokenList.prototype.replace = ClassList.prototype.replace;
-  }
-})();
-
 function Dropdown (element, initArr) {
   var element = element;
   var lastChoice = null;
+  var show = false;
+  var arr = null;
   this.setElements = setElements;
 
   /* INITIAL */
-  element.classList.add('dropdown');
-
   var inputArea = document.createElement('div');
-
-  inputArea.className = 'dropdown__input-area';
-
   var inputField = document.createElement('input');
-  inputField.type = 'text';
-  inputField.classList.add('dropdown__input-field');
-  inputArea.appendChild(inputField);
-
   var list = document.createElement('ul');
-  list.classList.add('dropdown__list');
-  element.appendChild(list);
 
-  inputField.addEventListener('input', search);
+  addCssClass(element, 'dropdown');
+  addCssClass(inputArea,'dropdown__input-area');
+  addCssClass(inputField,'dropdown__input-field');
+  addCssClass(list, 'dropdown__list');
 
   inputArea.addEventListener('click',function (event) {
-    element.querySelector('.dropdown__list').classList.contains('dropdown__list--show') ||
-    element.querySelector('.dropdown__list').classList.contains('dropdown__list--show-up')
-      ? hideList() : showList()
+    show ? hideList() : showList();
   });
+  inputArea.appendChild(inputField);
+
+  inputField.type = 'text';
+  inputField.addEventListener('input', search);
 
   element.appendChild(inputArea);
+  element.appendChild(list);
+
+  if (initArr) {
+    setElements(initArr);
+  }
+
+  /* Functions */
 
   function outsideClick(event) {
       if (!element.contains(event.target)){
@@ -109,33 +46,54 @@ function Dropdown (element, initArr) {
 
   function outsideEventRemove(){
     window.removeEventListener('click', outsideClick);
-    window.addEventListener('resize', hideList);
-    window.addEventListener('scroll', hideList);
+    window.removeEventListener('resize', hideList);
+    window.removeEventListener('scroll', hideList);
   }
 
-  if (initArr) {
-    setElements(initArr);
-  }
-
-  /* Functions */
-
-  function setElements (arr) {
+  function setElements (arrElements) {
     clearList();
+    arr = arrElements;
+    lastChoice = null;
     if (!arr) return;
     for (var i = 0; i < arr.length; i++) {
       var listItem = document.createElement('li');
       listItem.className = 'dropdown__list-item';
-      listItem.setAttribute( 'data-id', arr[i].id);
       listItem.innerHTML = arr[i].label;
-      listItem.addEventListener('click', function () {
-        lastChoice = listItem;
-        hideList();
-      });
+      listItem.addEventListener('click', {handleEvent: function () {
+          lastChoice = this.item;
+          hideList();
+        }, item: arr[i]} );
       list.appendChild(listItem);
     }
   }
 
+  function removeCssClass(element, cssClass){
+    var cssList = element.className.split(' ');
+    var indexToRemove = cssList.indexOf(cssClass);
+    if (indexToRemove === -1) {
+      return;
+    }
+    delete cssList[indexToRemove];
+    element.className = '';
+    for (var i = 0; i < cssList.length; i++){
+      if (cssList[i])
+        element.className += ' ' + cssList[i];
+    }
+    element.className = element.className.trim();
+  }
+
+  function addCssClass(element, cssClass){
+    var cssList = element.className.split(' ');
+    var index = cssList.indexOf(cssClass);
+    if (index !== -1) {
+      return;
+    }
+    element.className += ' ' + cssClass;
+    element.className = element.className.trim();
+  }
+
   function showList () {
+    show = true;
     inputField.value = '';
     inputField.focus();
     var height = element.getBoundingClientRect().bottom - element.getBoundingClientRect().top;
@@ -144,27 +102,27 @@ function Dropdown (element, initArr) {
     var listElementCount =
       list.childElementCount >= 5 ? 5 : list.childElementCount;
     if (windowHeight - bottom < height * listElementCount) {
-      list.classList.add('dropdown__list--show-up');
+      addCssClass(list,'dropdown__list--show-up');
     } else {
-      list.classList.add('dropdown__list--show');
+      addCssClass(list,'dropdown__list--show');
     }
     var listItems = list.querySelectorAll('.dropdown__list-item');
     for (var i = 0; i < listItems.length; i++) {
-      listItems[i].classList.remove('dropdown__list-item--hide');
+      removeCssClass(listItems[i], 'dropdown__list-item--hide')
     }
 
     outsideEventAdd();
   }
 
   function hideList () {
+    show = false;
     if (lastChoice) {
       var inputField = element.querySelector('.dropdown__input-field');
-      inputField.setAttribute('data-id', lastChoice.getAttribute('data-id'));
-      inputField.value = lastChoice.innerText;
+      inputField.value = lastChoice.label;
     }
     var list = element.querySelector('.dropdown__list');
-    list.classList.remove('dropdown__list--show');
-    list.classList.remove('dropdown__list--show-up');
+    removeCssClass(list,'dropdown__list--show');
+    removeCssClass(list,'dropdown__list--show-up');
     outsideEventRemove();
   }
 
@@ -178,12 +136,11 @@ function Dropdown (element, initArr) {
     var value = element
       .querySelector('.dropdown__input-field')
       .value.trim();
-
     var listItems = list.querySelectorAll('.dropdown__list-item');
     for (var i = 0; i < listItems.length; i++) {
-      listItems[i].innerText.startsWith(value)
-        ? listItems[i].classList.remove('dropdown__list-item--hide')
-        : listItems[i].classList.add('dropdown__list-item--hide')
+      value === '' || listItems[i].innerText.indexOf(value) === 0
+        ? removeCssClass(listItems[i],'dropdown__list-item--hide')
+        : addCssClass(listItems[i],'dropdown__list-item--hide')
     }
   }
 }
